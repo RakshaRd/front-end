fe846eeee9529e6b88c479248753e8bd2224c039
Raksha RD <rakshamanjula1@gmail.com>
2024-07-09 16:52:31 +0530

    LFS

---
+++ Clock/bharat-clock/src/components/clockHeading.jsx
@@ -0,0 +1,4 @@
+function ClockHeading() {
+  return <h1 className="fw-bolder">Bharat Clock</h1>;
+}
+export default ClockHeading;
---
+++ Clock/bharat-clock/src/components/currentTime.jsx
@@ -0,0 +1,10 @@
+function CurrentTime() {
+  let time = new Date();
+  return (
+    <p className="lead">
+      This is the current time:{time.toLocaleDateString()}-
+      {time.toLocaleTimeString()}
+    </p>
+  );
+}
+export default CurrentTime;
